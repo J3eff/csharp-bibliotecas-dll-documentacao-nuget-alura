2497fee8bd5a4e601b87e63bc297940eb76a14b3
J3eff <jeffersonvideo125@gmail.com>
2022-04-22 11:51:12 -0300

    Modificador de acesso internal

--- ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
+++ ByteBank.Modelos/Funcionarios/FuncionarioAutenticavel.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using ByteBank.Modelos;
-
+﻿
 namespace ByteBank.Modelos.Funcionarios
 {
     public abstract class FuncionarioAutenticavel : Funcionario, IAutenticavel
     {
+        private AutenticacaoHelper _autenticacaoHelper = new AutenticacaoHelper();
         public string Senha { get; set; }
 
         public FuncionarioAutenticavel(double salario, string cpf)
@@ -18,7 +13,7 @@ namespace ByteBank.Modelos.Funcionarios
 
         public bool Autenticar(string senha)
         {
-            return Senha == senha;
+            return _autenticacaoHelper.CompararSenhas(Senha, senha);
         }
     }
 }
